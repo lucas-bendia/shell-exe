fa17dba44eefbcc4a27ca3ea4fbf54b8c7698d40
lucas-bendia <146709490+lucas-bendia@users.noreply.github.com>
2023-10-06 11:28:29 +0200

    accessrights.sh

--- shell-exe/Job09/accessrights.sh
+++ shell-exe/Job09/accessrights.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+!/bin/bash
 
 for (( i=2; i<=14; i++ ))
 do
 	echo $i
 done
-       # role=$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 5 | sed 's/ //g' ) 
-        #user=$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 2 | sed 's/ //g' )-$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 3 | sed 's/ //g' )
-    	#mdp=$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 4 ) 
+       role=$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 5 | sed 's/ //g' ) 
+        user=$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 2 | sed 's/ //g' )-$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 3 | sed 's/ //g' )
+    	mdp=$( head -n $nb Shell_Userlist.csv | tail -n 1 | cut -d , -f 4 ) 
 
-	#sudo useradd $user 
-	#echo "$user:$mdp" | sudo chpasswd
+	sudo useradd $user 
+	echo "$user:$mdp" | sudo chpasswd
 
-	#if [[ "role" == *Admin* ]]; then 
-	#	sudo usermod -aG sudo $user 
-	#fi 
-#done
+	if [[ "role" == *Admin* ]]; then 
+	sudo usermod -aG sudo $user 
+	fi 
+done
  
